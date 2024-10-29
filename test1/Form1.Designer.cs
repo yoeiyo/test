e1785c8393ea0aeffe49b86b73457637f1bf112d
yoeiyo <106560105+yoeiyo@users.noreply.github.com>
2024-10-29 17:21:18 +0300

    тестовое задание

---
+++ test1/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inpFile_textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.reg_comboBox = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.inpFile_button = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.log_textBox = new System.Windows.Forms.TextBox();
+            this.log_button = new System.Windows.Forms.Button();
+            this.res_textBox = new System.Windows.Forms.TextBox();
+            this.res_button = new System.Windows.Forms.Button();
+            this.start_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Входной файл";
+            // 
+            // inpFile_textBox
+            // 
+            this.inpFile_textBox.Location = new System.Drawing.Point(12, 28);
+            this.inpFile_textBox.Name = "inpFile_textBox";
+            this.inpFile_textBox.Size = new System.Drawing.Size(331, 22);
+            this.inpFile_textBox.TabIndex = 1;
+            this.inpFile_textBox.TextChanged += new System.EventHandler(this.inpFile_textBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Район доставки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Время первой доставки";
+            // 
+            // reg_comboBox
+            // 
+            this.reg_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.reg_comboBox.FormattingEnabled = true;
+            this.reg_comboBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.reg_comboBox.Location = new System.Drawing.Point(12, 114);
+            this.reg_comboBox.Name = "reg_comboBox";
+            this.reg_comboBox.Size = new System.Drawing.Size(121, 24);
+            this.reg_comboBox.TabIndex = 4;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(235, 116);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker.TabIndex = 5;
+            // 
+            // inpFile_button
+            // 
+            this.inpFile_button.Location = new System.Drawing.Point(349, 24);
+            this.inpFile_button.Name = "inpFile_button";
+            this.inpFile_button.Size = new System.Drawing.Size(90, 30);
+            this.inpFile_button.TabIndex = 2;
+            this.inpFile_button.Text = "Обзор...";
+            this.inpFile_button.UseVisualStyleBackColor = true;
+            this.inpFile_button.Click += new System.EventHandler(this.inpFile_button_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Путь к лог-файлу";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(194, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Путь к файлу с результатом";
+            // 
+            // log_textBox
+            // 
+            this.log_textBox.Location = new System.Drawing.Point(12, 213);
+            this.log_textBox.Name = "log_textBox";
+            this.log_textBox.Size = new System.Drawing.Size(331, 22);
+            this.log_textBox.TabIndex = 8;
+            this.log_textBox.TextChanged += new System.EventHandler(this.log_textBox_TextChanged);
+            // 
+            // log_button
+            // 
+            this.log_button.Location = new System.Drawing.Point(349, 209);
+            this.log_button.Name = "log_button";
+            this.log_button.Size = new System.Drawing.Size(90, 30);
+            this.log_button.TabIndex = 9;
+            this.log_button.Text = "Обзор...";
+            this.log_button.UseVisualStyleBackColor = true;
+            this.log_button.Click += new System.EventHandler(this.log_button_Click);
+            // 
+            // res_textBox
+            // 
+            this.res_textBox.Location = new System.Drawing.Point(12, 285);
+            this.res_textBox.Name = "res_textBox";
+            this.res_textBox.Size = new System.Drawing.Size(331, 22);
+            this.res_textBox.TabIndex = 10;
+            this.res_textBox.TextChanged += new System.EventHandler(this.res_textBox_TextChanged);
+            // 
+            // res_button
+            // 
+            this.res_button.Location = new System.Drawing.Point(349, 281);
+            this.res_button.Name = "res_button";
+            this.res_button.Size = new System.Drawing.Size(90, 30);
+            this.res_button.TabIndex = 11;
+            this.res_button.Text = "Обзор...";
+            this.res_button.UseVisualStyleBackColor = true;
+            this.res_button.Click += new System.EventHandler(this.res_button_Click);
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(179, 360);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(90, 30);
+            this.start_button.TabIndex = 12;
+            this.start_button.Text = "Старт";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 402);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.res_button);
+            this.Controls.Add(this.res_textBox);
+            this.Controls.Add(this.log_button);
+            this.Controls.Add(this.log_textBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.inpFile_button);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.reg_comboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inpFile_textBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "служба доставки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inpFile_textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox reg_comboBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button inpFile_button;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox log_textBox;
+        private System.Windows.Forms.Button log_button;
+        private System.Windows.Forms.TextBox res_textBox;
+        private System.Windows.Forms.Button res_button;
+        private System.Windows.Forms.Button start_button;
+    }
+}
+
---
+++ test1/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace test1
+{
+    // одна строка входных данных
+    struct data
+    {
+        public int id { get; set; }
+        public int weight { get; set; }
+        public int region { get; set; }
+        public DateTime time { get; set; }
+
+        public data(string[] s)
+        {
+            id = Convert.ToInt32(s[0]);
+            weight = Convert.ToInt32(s[1]);
+            region = Convert.ToInt32(s[2]);
+            time = DateTime.ParseExact(s[3] + " " + s[4], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString() => $"{id} {weight} {region} {time}";
+    }
+
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog fbd;
+        OpenFileDialog ofd;
+
+        // входной, лог и результирующий файлы соответственно
+        string fin = "";
+        string log = Directory.GetCurrentDirectory();
+        string res = Directory.GetCurrentDirectory();
+
+        // флаг, обозначающий были ли фходные данные уже считаны из файла
+        bool inpRead;
+
+        // списки со всеми данными из файла и результатом фильтрации
+        List<data> dataList = new List<data>();
+        List<data> resList = new List<data>();
+
+        TimeSpan span = new TimeSpan(0, 30, 0);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            log_textBox.Text = log;
+            res_textBox.Text = res;
+            inpRead = false;
+        }
+
+        private void inpFile_button_Click(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = Directory.GetCurrentDirectory();
+            ofd.Filter = "txt files (*.txt)|*.txt";
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK && fin != ofd.FileName)
+            {
+                fin = ofd.FileName;
+                inpFile_textBox.Text = fin;
+
+                if (inpRead)
+                {
+                    dataList.Clear();
+                    resList.Clear();
+                    inpRead = false;
+                }
+            }
+        }
+
+        private void log_button_Click(object sender, EventArgs e)
+        {
+            fbd = new FolderBrowserDialog();
+
+            if (Directory.Exists(log))
+                fbd.SelectedPath = log;
+            else
+                fbd.SelectedPath = Directory.GetCurrentDirectory();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                log = fbd.SelectedPath;
+                log_textBox.Text = log;
+            }
+        }
+
+        private void inpFile_textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (fin != inpFile_textBox.Text)
+            {
+                fin = inpFile_textBox.Text;
+                if (inpRead)
+                {
+                    dataList.Clear();
+                    resList.Clear();
+                    inpRead = false;
+                }
+            }   
+        }
+
+        private void log_textBox_TextChanged(object sender, EventArgs e)
+        {
+            log = log_textBox.Text;
+        }
+
+        private void res_textBox_TextChanged(object sender, EventArgs e)
+        {
+            res = res_textBox.Text;
+        }
+
+        private void res_button_Click(object sender, EventArgs e)
+        {
+            fbd = new FolderBrowserDialog();
+
+            if (Directory.Exists(res))
+                fbd.SelectedPath = res;
+            else
+                fbd.SelectedPath = Directory.GetCurrentDirectory();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                res = fbd.SelectedPath;
+                res_textBox.Text = res;
+            }
+        }
+
+        // обработка ошибок при заполнении формы
+        private bool errorHand()
+        {
+            if (fin == "")
+            {
+                MessageBox.Show("Не задан входной файл");
+                return false;
+            }
+            if (!File.Exists(fin))
+            {
+                MessageBox.Show("Заданный входной файл не существует");
+                return false;
+            }
+            if (Path.GetExtension(fin) != ".txt")
+            {
+                MessageBox.Show("Неверное расширение входного файла");
+                return false;
+            }
+            if (reg_comboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Не выбран район доставки");
+                return false;
+            }
+            if (!Directory.Exists(log))
+            {
+                MessageBox.Show("Неверный путь к лог-файлу");
+                return false;
+            }
+            if (!Directory.Exists(res))
+            {
+                MessageBox.Show("Неверный путь к файлу с результатом");
+                return false;
+            }
+
+            return true;
+        }
+
+        // чтение входного файла
+        private bool readInp(TextWriter logWriter)
+        {
+            bool fl = true;
+            data d;
+
+            using (FileStream fs = new FileStream(fin, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    logWriter.Write($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}  ");
+                    logWriter.WriteLine($"Открыт входной файл {fin}");
+
+                    while (!sr.EndOfStream && fl)
+                    {
+                        string[] s = sr.ReadLine().Split(' ');
+                        if (s.Length != 5)
+                        {
+                            MessageBox.Show("Ошибка ввода: неверная строка с данными");
+                            logWriter.Write($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}  ");
+                            logWriter.WriteLine("Ошибка ввода: неверная строка с данными");
+                            fl = false;
+                        }
+                        else
+                        {
+                            try
+                            {
+                                d = new data(s);
+
+                                var unique = dataList.Where(x => x.id == d.id);
+
+                                if (unique.Any())
+                                    throw new Exception("Ошибка: идентификатор не уникальный");
+                                else
+                                {
+                                    dataList.Add(d);
+
+                                    // так как это первое считывание файла, входные данные сразу фильтруются
+                                    if (d.region == Convert.ToInt32(reg_comboBox.SelectedItem) && d.time >= dateTimePicker.Value && d.time <= dateTimePicker.Value + span)
+                                        resList.Add(d);
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                MessageBox.Show(e.Message);
+                                logWriter.Write($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()} ");
+                                logWriter.WriteLine(e.Message);
+                                fl = false;
+                            }
+                        }
+                    }
+                }
+            }
+
+            logWriter.Write($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}  ");
+            if (fl)
+                logWriter.WriteLine($"Входной файл успешно прочитан, данные отфильтрованы по району {Convert.ToInt32(reg_comboBox.SelectedItem)} и времени первой доставки {dateTimePicker.Value}");
+            else
+            {
+                logWriter.WriteLine("При чтении входнного файла возникли ошибки, данные не были обработаны");
+                dataList.Clear();
+                resList.Clear();
+            }
+            return fl;
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            if (errorHand())
+            {
+                using (StreamWriter logFileWriter = new StreamWriter(log + "\\log.txt", true))
+                {
+                    if (inpRead)
+                    {
+                        // фильтрация данных, если входной файл уже был считан
+                        resList = dataList.Where(x => x.region == Convert.ToInt32(reg_comboBox.SelectedItem) && x.time >= dateTimePicker.Value && x.time <= dateTimePicker.Value + span).ToList();
+
+                        logFileWriter.Write($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}  ");
+                        logFileWriter.WriteLine($"Данные отфильтрованы по району {Convert.ToInt32(reg_comboBox.SelectedItem)} и времени первой доставки {dateTimePicker.Value}");
+                    }
+                    else
+                        inpRead = readInp(logFileWriter);
+
+                    if (inpRead)
+                    {
+                        using (FileStream fs = new FileStream(res + "\\res.txt", FileMode.Create, FileAccess.Write))
+                        {
+                            using (StreamWriter sw = new StreamWriter(fs))
+                            {
+                                logFileWriter.Write($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()} ");
+                                logFileWriter.WriteLine("Открыт файл с результатом");
+
+                                foreach (var item in resList)
+                                {
+                                    sw.WriteLine(item.ToString());
+                                }
+
+                                logFileWriter.Write($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()} ");
+                                logFileWriter.WriteLine($"Результат фильтрации ({resList.Count} строк) успешно записан в файл {res}\\res.txt");
+                                MessageBox.Show($"Результат фильтрации ({resList.Count} строк) успешно записан в файл {res}\\res.txt");
+                            }
+                        }
+                    }
+                }
+            }
+                
+        }
+    }
+}
